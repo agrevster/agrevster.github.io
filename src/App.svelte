7b9ff6cf34535fc8f12b7608507dbdef72d7f427
agrevster <138630000+agrevster@users.noreply.github.com>
2023-09-05 13:10:02 -0500

    Added nametag and picture
    Signed-off-by: agrevster <138630000+agrevster@users.noreply.github.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,41 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    import portrait from './assets/portrait.png'
+    import viteLogo from '/vite.svg'
+    import Counter from './lib/Counter.svelte'
+    import Divider from "./lib/Divider.svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@200;400;800&display=swap" rel="stylesheet">
+    <div class="card">
+        <img id="portrait" src="{portrait}" alt="" title="An AI generated lighthouse picture">
+        <p id="nametag">Hello, I'm Alex
+            <br>
+            <span id="description">[Developer and barbeque enthusiast]</span>
+        </p>
+        <Divider/>
+    </div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    #nametag {
+        align-self: flex-start;
+        font-family: 'JetBrains Mono', monospace;
+        font-weight: 800;
+        font-size: 1.5rem;
+    }
+    #portrait {
+        margin-top: 1em;
+        width: 10em;
+        height: 10em;
+        border-radius: 4.0em;
+        box-shadow: .5em .5em var(--deep_ocean);
+    }
+    #description {
+        font-family: 'JetBrains Mono', monospace;
+        font-weight: 400;
+        font-size:1.0rem;
+    }
 </style>
---
+++ src/lib/Divider.svelte
@@ -0,0 +1,8 @@
+<div class="divider"></div>
+
+<style>
+    .divider{
+        border-top: .5em dashed var(--white_wash);
+        padding-bottom: 1em;
+    }
+</style>
